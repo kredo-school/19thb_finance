b3fdb3300ed8b6eb272d5872c1077b4f997d5832
Mari <nocturne8410@gamil.com>
2024-04-23 21:56:51 +0900

    Add background-colour of app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,20 +21,20 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
-<body>
 <header class="color-background" style="height: 80px;">
     @include('components.header')
 </header>
 
+<body class="bg-color-Background">
     <div id="app" style="min-height: calc(100vh - 160px);">
         <main>
             @yield('content')
         </main>
     </div>
+</body>
 
 <footer class="footer bg-color-Letter">
     @include('components.footer')
 </footer>
-</body>
 
 </html>
