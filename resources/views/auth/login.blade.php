08ace4ac509f5f12677504c26a44e60a8053520b
Mari <nocturne8410@gamil.com>
2024-04-30 22:40:33 +0900

    Add images

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,14 +6,14 @@
 <div class="container-fluid position-relative m-0 p-0">
     <img src="images/bg_img_signin_register.jpg" alt="bg_img_signin_register" class="w-100" style="height: 140px;">
 
-    <p class="h2 fw-bold position-absolute top-50 start-50 translate-middle">Welcome to Money-Juu</p>
+    <p class="h2 fw-bold color-Yellow position-absolute top-50 start-50 translate-middle" style="letter-spacing: .2rem;">Welcome to Money-Juu</p>
 </div>
 
-<div class="container py-4">
+<div class="container my-4">
     <div class="row justify-content-center">
         <div class="col-md-auto position-relative">
             <div class="row">
-                <h2 class="h4 text-center border border-3 rounded-3 mx-3 px-3 py-1" style="max-width: 150px;">{{ __('Sign In') }}</h2>
+                <h2 class="h4 text-center border border-3 rounded-3 shadow bg-white bg-opacity-50 mx-3 px-3 py-1" style="max-width: 150px;">{{ __('Sign In') }}</h2>
             </div>
 
             <div class="row position-absolute bottom-0 end-0">
@@ -26,7 +26,7 @@
         
         <div class="col-md-auto d-flex align-items-stretch">
             <div class="card mx-3 p-0">
-                <div class="card-body border border-3 px-5 py-4" style="max-width: 500px;">
+                <div class="card-body border border-3 px-5 py-4 bg-color-Background" style="max-width: 500px;">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
@@ -34,7 +34,7 @@
                             <label for="email" class="col-form-label text-md-start py-0">{{ __('Email') }}</label>
 
                             <div>
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" placeholder="&#xf003;" style="font-family: FontAwesome;" required autocomplete="email" autofocus>
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -48,7 +48,7 @@
                             <label for="password" class="col-form-label text-md-start py-0">{{ __('Password') }}</label>
 
                             <div>
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" placeholder="&#xf023;" style="font-family: FontAwesome;" required autocomplete="current-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -60,7 +60,7 @@
 
                         <div class="row mb-0 p-0">
                             <div class="mb-2">
-                                <button type="submit" class="btn btn-primary rounded-pill w-100">
+                                <button type="submit" class="btn fw-bold color-Background bg-color4 rounded-pill w-100" style="letter-spacing: .1rem;">
                                     {{ __('Sign In') }}
                                 </button>
                             </div>
@@ -75,16 +75,16 @@
                                 </div>
 
                                 @if (Route::has('password.request'))
-                                <a class="btn btn-link text-decoration-none text-md-start d-inline" href="{{ route('password.request') }}">
+                                <a class="btn btn-link text-decoration-none fw-semibold color4 text-md-start d-inline" href="{{ route('password.request') }}">
                                     {{ __('Forgot Password?') }}
                                 </a>
                                 @endif
 
-                                <a class="btn btn-link text-decoration-none text-md-start d-block" href="#">
+                                <a class="btn btn-link text-decoration-none fw-semibold color4 text-md-start d-block" href="#">
                                     {{ __('Create an Account') }}
                                 </a>
 
-                                <a class="btn btn-link text-decoration-none text-md-start d-block" href="#">
+                                <a class="btn btn-link text-decoration-none fw-semibold color4 text-md-start d-block" href="#">
                                     {{ __('Privacy & Terms') }}
                                 </a>
                             </div>
