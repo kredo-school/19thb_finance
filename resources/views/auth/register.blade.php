6176d6ecb0d87f0fe9b2b27e9ce6998b9929ff48
Mari <nocturne8410@gamil.com>
2024-05-04 19:01:48 +0900

    Added landing page changed button colors

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 <div class="container-fluid position-relative m-0 p-0">
-    <img src="images/bg_img_signin_register.jpg" alt="bg_img_signin_register" class="w-100" style="height: 140px;">
+    <img src="images/bg_img_signin_register.jpg" alt="background_register" class="w-100" style="height: 140px;">
 
     <p class="h2 fw-bold color-Yellow position-absolute top-50 start-50 translate-middle" style="letter-spacing: .2rem;">Welcome to Money-Juu</p>
 </div>
